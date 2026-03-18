e3282e7cd2f93352ebce4dce7a3a71b5b0240777
AlAminAshik <alaminashik890.bd@gmail.com>
2026-03-18 19:03:56 +0600

    .

--- Basic_codes_101/src/GPIOBasic.cpp
+++ Basic_codes_101/src/GPIOBasic.cpp
@@ -18,17 +18,16 @@
 //     Serial.println((analogRead(P1)));
 // }
 
-// /*for extended GPIO
-// #include "unihiker_k10.h"
+// for extended GPIO
+#include "unihiker_k10.h"
 
-// UNIHIKER_K10 k10;
+UNIHIKER_K10 k10;
 
-// void setup() {
-//     k10.begin();
-//     Serial.begin(9600);
-//     digital_write(eP2, HIGH);
-// }
-// void loop() {
-//     Serial.println((digital_read(eP3)));
-// }
-// */
+void setup() {
+    k10.begin();
+    Serial.begin(9600);
+    digital_write(eP2, HIGH);
+}
+void loop() {
+    Serial.println((digital_read(eP3)));
+}
